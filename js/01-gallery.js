b125546ea0338d0029338c97e14d0f64751b3742
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-03-18 14:49:07 +0000

    Task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Разбей его на несколько подзадач:
+
+// 1.Создание и рендер разметки по массиву данных galleryItems 
+// и предоставленному шаблону элемента галереи.
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
+// Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
+// 4.Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией 
+// и примерами.
+ // 5.Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
+// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+
+
+const galleryDivEl = document.querySelector('.gallery')
+const galleryItemsToString = galleryItems.reduce((acc, {original,preview, description}) => {
+    return acc+=`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div> `
+}, '');
+galleryDivEl.insertAdjacentHTML('beforeend', galleryItemsToString)
+
+
+
+galleryDivEl.onclick = (event) => {
+  event.preventDefault();
+   if (event.target.nodeName !== "IMG") {
+        return
+   }
+  const elementDatasetSource = event.target.dataset.source;
+  const html = `
+		<img src="${elementDatasetSource}" width="800" height="600">
+	`;
+
+  const instance = basicLightbox.create(html, {
+    onShow: (instance) => window.addEventListener("keydown", onEscKeydown),
+    onClose: (instance) => window.removeEventListener("keydown",onEscKeydown)
+  })
+  instance.show((instance) => console.log('finished show()', instance))
+
+
+  function onEscKeydown(event) {
+    if (event.code !== "Escape") {
+            return
+        }
+    instance.close()
+  }
+}
+  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// 1.Создание и рендер разметки по массиву данных galleryItems 
+// и предоставленному шаблону элемента галереи.Используй готовый код из первого задания.
+// 2.Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs.
+// Необходимо добавить ссылки на два файла: simple - lightbox.min.js и simple - lightbox.min.css.
+// 3.Инициализация библиотеки после того как элементы галереи созданы 
+// и добавлены в ul.gallery.Для этого ознакомься с документацией SimpleLightbox -
+// в первую очередь секции «Usage» и «Markup».
+// 4.Посмотри в документации секцию «Options» и добавь отображение 
+// подписей к изображениям из атрибута alt.Пусть подпись будет снизу и появляется 
+// через 250 миллисекунд после открытия изображения.
+
+const galleryEl = document.querySelector(".gallery")
+const galleryItemsToString = galleryItems.reduce((acc, {preview, original, description}) => {
+    return acc+=`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+        />
+   </a>
+</li>`
+}, "")
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsToString)
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionPosition:"bottom", captionDelay: 250,  scrollZoom: false});
+
+gallery.on('shown.simplelightbox', function (event) {
+//    event.preventDefault(),
+  console.log("Everything works fine")
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e, "Something wrong"); 
+});
